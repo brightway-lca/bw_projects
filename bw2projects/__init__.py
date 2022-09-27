0cebe54a9c3c4ac874e521183729d22bb2d54cb4
Shantanu <shantanu@senic.com>
2022-09-27 09:43:18 +0530

    Cleanup preferences related part of projects

--- bw2projects/__init__.py
+++ bw2projects/__init__.py
@@ -4,9 +4,3 @@ from .version import version as __version__
 
 from .configuration import config
 from .project import projects, ProjectDataset
-from .meta import Preferences
-
-preferences = Preferences(projects.dir)
-config.metadata.extend(preferences)
-
-config.p = preferences
