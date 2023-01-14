4c634f1ca75b7a7bfd6e427b0adfb4d276e1ac6a
Shantanu <shantanu@senic.com>
2023-01-14 13:06:43 +0530

    Make SubstitutableDatabase available for import from projects

--- brightway_projects/__init__.py
+++ brightway_projects/__init__.py
@@ -2,6 +2,7 @@
 from brightway_projects.utils import get_version_tuple
 from brightway_projects.configuration import config
 from brightway_projects.project import projects, ProjectDataset
+from brightway_projects.sqlite import SubstitutableDatabase
 
 __all__ = (
     "__version__",
@@ -9,6 +10,7 @@ __all__ = (
     "config",
     "projects",
     "ProjectDataset",
+    "SubstitutableDatabase",
 )
 
 __version__ = get_version_tuple()
