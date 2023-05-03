fd05d17be9ca021633180c789c7953e00668525e
sami-m-g <sami.mg@outlook.com>
2023-05-03 03:53:06 +0300

    bw_projects: removed utils.py and version

--- bw_projects/__init__.py
+++ bw_projects/__init__.py
@@ -1,17 +1,34 @@
 """bw_projects."""
-from bw_projects.utils import get_version_tuple
+import importlib.metadata
+from typing import Union
+
 from bw_projects.configuration import config
-from bw_projects.project import projects, ProjectDataset
+from bw_projects.project import ProjectDataset, projects
 from bw_projects.sqlite import SubstitutableDatabase
 
 __all__ = (
     "__version__",
-    # Add functions and variables you want exposed in `bw_projects.`
-    # namespace here
     "config",
     "projects",
     "ProjectDataset",
     "SubstitutableDatabase",
 )
+
+
+def get_version_tuple() -> tuple:
+    """Returns version as (major, minor, micro)."""
+
+    def as_integer(version: str) -> Union[int, str]:
+        """Tries parsing version else returns as is."""
+        try:
+            return int(version)
+        except ValueError:  # pragma: no cover
+            return version  # pragma: no cover
+
+    return tuple(
+        as_integer(v)
+        for v in importlib.metadata.version("bw_projects").strip().split(".")
+    )
+
 
 __version__ = get_version_tuple()
